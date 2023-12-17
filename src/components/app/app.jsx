ddd1f4349aebab7f75030290710ecd734cf26980
visetin <visetin9@yandex.ru>
2023-12-18 01:59:58 +0300

    fix: fix app layout styles

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,18 +1,20 @@
 import '@ya.praktikum/react-developer-burger-ui-components/dist/ui/common.css';
 import '@ya.praktikum/react-developer-burger-ui-components/dist/ui/box.css';
-import AppHeader from '../app-header/app-header'
+import AppHeader from '../app-header/app-header';
+import BurgerIngredients from '../burger-ingredients/burger-ingredients';
+import { ingredientsMockData } from '../../utils/data';
 import './app.css';
 
 const App = () => {
   return (
     <>
       <AppHeader />
-      <main className="container row p-10">
-        <div className="col">
+      <main className="page-container page-layout page-layout_cropped pl-5 pt-10 pr-5 pb-10">
+        <div className="page-col page-col_cropped">
           <h1 className="text text_type_main-large mb-5">Соберите бургер</h1>
-          constructor
+          <BurgerIngredients ingredients={ingredientsMockData} />
         </div>
-        <div className="col">ingredients</div>
+        <div className="page-col page-col_cropped">constructor</div>
       </main>
     </>
   )
