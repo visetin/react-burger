f9f77b0a451bc4cb42a6d85849fc5c37ab242402
visetin <visetin9@yandex.ru>
2023-12-23 19:53:13 +0300

    feat: extend burger-constructor layout

--- src/components/burger-constructor/burger-constructor.js
+++ src/components/burger-constructor/burger-constructor.js
@@ -1,19 +1,26 @@
 import '@ya.praktikum/react-developer-burger-ui-components/dist/ui/common.css';
 import '@ya.praktikum/react-developer-burger-ui-components/dist/ui/box.css';
-import { ConstructorElement, CurrencyIcon, Button } from '@ya.praktikum/react-developer-burger-ui-components';
+import { ConstructorElement, DragIcon, CurrencyIcon, Button } from '@ya.praktikum/react-developer-burger-ui-components';
 
 const ELEMENT_TEMP_IMG = 'https://code.s3.yandex.net/react/code/bun-02.png';
 
 const makeMockIngredientsList = () => {
   const result = [];
 
-  for (let index = 0; index < 10; index += 1) {
-    result.push(<ConstructorElement
-      key={index}
-      text="Краторная булка N-200i"
-      price={50}
-      thumbnail={ELEMENT_TEMP_IMG}
-    />);
+  for (let index = 0; index < 8; index += 1) {
+    result.push(
+      <div className="relative pl-8 pr-2" key={index}>
+        <div className="absolute pos-left pos-middle flex-row">
+          <DragIcon type="primary" />
+        </div>
+
+        <ConstructorElement
+          text="Краторная булка N-200i"
+          price={50}
+          thumbnail={ELEMENT_TEMP_IMG}
+        />
+      </div>
+    );
   }
 
   return result;
@@ -25,13 +32,15 @@ const BurgerConstructor = () => {
   return (
     <>
       <div className="flex-col h-100p cropper mb-10">
-        <ConstructorElement
-          type="top"
-          isLocked={true}
-          text="Краторная булка N-200i (верх)"
-          price={200}
-          thumbnail={ELEMENT_TEMP_IMG}
-        />
+        <div className="pl-8 pr-5">
+          <ConstructorElement
+            type="top"
+            isLocked={true}
+            text="Краторная булка N-200i (верх)"
+            price={200}
+            thumbnail={ELEMENT_TEMP_IMG}
+          />
+        </div>
 
         <div className="scroll custom-scroll h-100p mt-4 mb-4">
           <div className="flex-col gap-y-4">
@@ -39,18 +48,20 @@ const BurgerConstructor = () => {
           </div>
         </div>
 
-        <ConstructorElement
-          type="bottom"
-          isLocked={true}
-          text="Краторная булка N-200i (верх)"
-          price={200}
-          thumbnail={ELEMENT_TEMP_IMG}
-        />
+        <div className="pl-8 pr-5">
+          <ConstructorElement
+            type="bottom"
+            isLocked={true}
+            text="Краторная булка N-200i (верх)"
+            price={200}
+            thumbnail={ELEMENT_TEMP_IMG}
+          />
+        </div>
       </div>
 
       <div className="flex-row flex-jc-end flex-ai-center">
         <span className="text text_type_digits-medium mr-2">610</span>
-        <span className="icon-32 pr-10">
+        <span className="icon-8 pr-10">
           <CurrencyIcon type="primary" size="32" />
         </span>
         <Button htmlType="button" type="primary" size="large">
