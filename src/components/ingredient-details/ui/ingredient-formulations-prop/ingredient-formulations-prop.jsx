e3f0987fd472b0ec33ad4daba0f1141abbd305d0
visetin <visetin9@yandex.ru>
2023-12-23 23:42:23 +0300

    fix: fix ingredient-formulations-prop styles

--- src/components/ingredient-details/ui/ingredient-formulations-prop/ingredient-formulations-prop.jsx
+++ src/components/ingredient-details/ui/ingredient-formulations-prop/ingredient-formulations-prop.jsx
@@ -5,7 +5,7 @@ const IngredientFormulationsProp = ({ title, value }) => {
   return (
     <div className={ingredientFormulationsPropStyles.item}>
       <p className="text-center text text_type_main-default text_color_inactive mb-2">{title}</p>
-      <p className="text-center text text_type_main-default text_color_inactive">{value}</p>
+      <p className="text-center text text_type_digits-default text_color_inactive">{value}</p>
     </div>
   )
 };
