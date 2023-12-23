1aac6a83ff8abe2e96ae1a36a1aa41af3699df3b
visetin <visetin9@yandex.ru>
2023-12-23 19:52:50 +0300

    refactor: extend project styles & change app styles

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -53,16 +53,18 @@ const App = () => {
   return (
     <>
       <AppHeader />
-      <main className="container cropper page-layout page-layout_cropped pl-5 pt-10 pr-5 pb-10">
-        <div className="page-slot flex-col">
-          <h1 className="text text_type_main-large mb-5">Соберите бургер</h1>
-          <div className="cropper h-100p">
-            <BurgerIngredients ingredients={categorizedIngredients} />
+      <main className="page cropper pl-5 pt-10 pr-5 pb-10 page_cropped">
+        <div className="container cropper h-100p flex-row gap-x-10">
+          <div className="flex-col page__slot">
+            <h1 className="text text_type_main-large mb-5">Соберите бургер</h1>
+            <div className="cropper h-100p">
+              <BurgerIngredients ingredients={categorizedIngredients} />
+            </div>
           </div>
-        </div>
 
-        <div className="page-slot flex-col pt-25 pb-18 pr-4 pl-4">
-          <BurgerConstructor />
+          <div className="flex-col pt-25 pb-18 pr-4 pl-4 page__slot">
+            <BurgerConstructor />
+          </div>
         </div>
       </main>
     </>
